e6cd0c95d9632c6fc91f4cbfcfea3742a2bb5496
Juan Acosta <jash.4060@gmail.com>
2025-06-03 23:19:46 -0500

    feat: improve characther card style

--- app/src/main/java/com/laura/marvel/adapters/CharacterAdapter.java
+++ app/src/main/java/com/laura/marvel/adapters/CharacterAdapter.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,14 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.laura.marvel.CharacterDetails;
 import com.laura.marvel.R;
 import com.laura.marvel.classes.Characters;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.ViewHolder> {
     private List<Characters> datos;
@@ -47,32 +45,43 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.View
         return datos.size();
     }
 
-    public class ViewHolder extends  RecyclerView.ViewHolder {
-        Button btn_name;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_name;
         ImageView img_details;
+        FloatingActionButton btn_favorite;
         TextView txt_desc, txt_id, txt_comics, txt_series, txt_stories;
+        View itemContainer;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            btn_name = itemView.findViewById(R.id.txt_name);
+            txt_name = itemView.findViewById(R.id.txt_name);
             txt_desc = itemView.findViewById(R.id.txt_desc);
             txt_id = itemView.findViewById(R.id.txt_id);
-            txt_comics = itemView.findViewById(R.id.item_comic);
+            txt_comics = itemView.findViewById(R.id.txt_comics);
             txt_series = itemView.findViewById(R.id.txt_series);
             txt_stories = itemView.findViewById(R.id.txt_stories);
             img_details = itemView.findViewById(R.id.img_pfp);
+            btn_favorite = itemView.findViewById(R.id.btn_favorite);
+            itemContainer = itemView;
         }
 
         public void bind(Characters character) {
-            btn_name.setText(character.getName());
+            txt_name.setText(character.getName());
             txt_desc.setText(character.getDesc());
             Picasso.get().load(character.getImg()).into(img_details);
-            System.out.println("----" + character.getImg());
-//            txt_comics.setText(character.getComics());
-//            txt_series.setText(character.getSeries());
-//            txt_stories.setText(character.getStories());
-
-            btn_name.setOnClickListener(new View.OnClickListener() {
+            
+            // Manejo del evento de clic en el botón favorito
+            btn_favorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Aquí se puede implementar la lógica para marcar como favorito
+                    // Por ejemplo, cambiar el icono o guardar en una lista de favoritos
+                    btn_favorite.setImageResource(android.R.drawable.btn_star_big_on);
+                }
+            });
+            
+            // Hacer que toda la tarjeta sea clickeable para ver detalles
+            itemContainer.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent i = new Intent(v.getContext(), CharacterDetails.class);
